582b4900728f441c95c3e0155586eb313ca8f765
Jordan Martinetti <jordan@jordanmartinetti.com>
2023-12-13 21:10:14 -0600

    Initial Commit

---
+++ Ions.R
@@ -0,0 +1,4 @@
+suppressMessages(library(tidyverse))
+
+df <- read.csv("https://query.data.world/s/a7daatpbbqbht722grkkm3ws522yhg?dws=00000",
+               header = TRUE, stringsAsFactors = FALSE)
